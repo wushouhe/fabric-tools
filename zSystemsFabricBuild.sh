c1285e0d4a42a5601d3e98e247fae6ac521c56a8
John Harrison <jharriso@us.ibm.com>
2016-08-31 08:22:12 -0400

    Use hyperledger-archives/fabric repo

--- zSystemsFabricBuild.sh
+++ zSystemsFabricBuild.sh
@@ -439,7 +439,8 @@ build_hyperledger_core() {
   # Delete fabric directory, if it exists
   rm -rf fabric
   # git clone http://gerrit.hyperledger.org/r/fabric.git
-  git clone https://github.com/hyperledger/fabric.git
+  # git clone https://github.com/hyperledger/fabric.git
+  git clone https://github.com/hyperledger-archives/fabric
 
   # Build the Hyperledger Fabric core components
   if [ $USE_DOCKER_HUB -eq 0 ] || [ $OS_FLAVOR == "sles" ]; then
