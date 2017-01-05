7ed1b5b7649084914b8c0b6621b6971337fa09db
John Harrison <jharriso@us.ibm.com>
2017-01-04 23:06:28 -0500

    Added additional python modules

--- zSystemsFabricBuild-v1.0.sh
+++ zSystemsFabricBuild-v1.0.sh
@@ -244,10 +244,10 @@ setup_behave() {
   python get-pip.py > /dev/null 2>&1
   pip install -q --upgrade pip > /dev/null 2>&1
   pip install -q behave nose docker-compose > /dev/null 2>&1
-  pip install -q -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3 > /dev/null 2>&1
+  pip install -q -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3 pyOpenSSL==16.2.0 sha3==0.2.1 > /dev/null 2>&1
   pip uninstall -yq six==1.3.0 > /dev/null 2>&1
 
-  # INstall protobuf and grpcio
+  # Install protobuf and grpcio
   git clone https://github.com/grpc/grpc.git
   cd grpc
   pip install -rrequirements.txt
